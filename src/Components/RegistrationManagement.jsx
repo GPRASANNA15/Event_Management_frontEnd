0ed980ca44de3f04ae115aa98d861961e2924ce3
Prasanna G <prasannag1505@gmail.com>
2026-03-06 02:18:04 +0530

    Update RegistrationManagement.jsx

--- src/Components/RegistrationManagement.jsx
+++ src/Components/RegistrationManagement.jsx
@@ -13,19 +13,19 @@ function RegistrationManagement() {
     },[]);
     return (
         <>
-            <div class="container mt-4">
+            <div className="container mt-4">
                 {details.length>0?(
-                <div class="card" style="max-width: 400px; margin:auto;">
+                <div className="card" style="max-width: 400px; margin:auto;">
                  {details.map(det=>(
-                    <div class="card-body">
-                        <h5 class="card-title text-center mb-3">Registration Details</h5>
+                    <div className="card-body">
+                        <h5 className="card-title text-center mb-3">Registration Details</h5>
 
                         <p><strong>Registered ID:</strong> {det.registeredId}</p>
                         <p><strong>Event Title:</strong> {det.eventName}</p>
                         <p><strong>Registered At:</strong> {det.registeredAt}</p>
                         <p><strong>User name:</strong>{det.userName} </p>
                         <p>
-                            <strong>Status:</strong><span class="badge bg-primary">{det.status}</span>
+                            <strong>Status:</strong><span className="badge bg-primary">{det.status}</span>
                         </p>
                     
 
